f531ed39ae3fa3711af349b4976b008cfd6f8c9e
Justin Tran <justintran@cma-100-64-27-105.resnet.ucsc.edu>
2025-10-08 01:20:42 -0700

    mysql database implemented

--- backend/NBApredictions/settings.py
+++ backend/NBApredictions/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backendApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'NBApredictions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nbapredictor',
+        'USER': 'root',
+        'PASSWORD': 'Justin041906',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
